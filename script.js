6a9ce4c19d99d3a5232963d91e0ea2f50f176311
John <jbhumph@gmail.com>
2024-05-30 16:42:13 -0700

    Final UI UX tweaks. Should now be complete.

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 // Declare game variables
-let humanScore = 0;
-let computerScore = 0;
+let humanScore = 9;
+let computerScore = 9;
 let choice = null;
 let responseLine = "";
+let winner = null;
+let element = 0
 
 // Declare document items as variables
 const rock = document.querySelector("#rock");
@@ -12,6 +14,13 @@ const play = document.querySelector("#play");
 const response = document.querySelector(".response");
 const scoreboard = document.querySelector(".scoreboard");
 const banner = document.querySelector(".banner");
+const image = document.querySelector("#image");
+
+scoreboard.textContent = `Player: 0   Computer: 0`;
+response.textContent = "Let's play!";
+
+image.src = "images/start-game.jpg";
+image.style = "border:5px solid black; border-radius:8px";
 
 rock.addEventListener("click", () => {
     playRound("Rock");
@@ -31,9 +40,12 @@ play.addEventListener("click", () => {
     choice = null;
     responseLine = "";
 
-    response.textContent = responseLine;
-    scoreboard.textContent = "";
-    banner.textContent = "";
+    response.textContent = "Make your move...";
+    scoreboard.textContent = `Player: ${humanScore}   Computer: ${computerScore}`;
+    banner.textContent = "Game on!";
+
+    image.src = "images/start-game.jpg";
+    image.style = "border:5px solid black; border-radius:8px";
 
 })
 
@@ -53,39 +65,60 @@ function getComputerChoice() {
 function playRound(humanChoice) {
     if (humanScore <= 4 && computerScore <= 4) {
         computerChoice = getComputerChoice();
+        let object = 0;
         if (humanChoice == computerChoice) {
             responseLine = "Tie!";
         } else if (computerChoice == "Rock") {
             if (humanChoice == "Paper") {
                 humanScore++;
                 responseLine = "You Win! Paper beats rock.";
+                object = 2;
             } else if (humanChoice == "Scissors") {
                 computerScore++;
                 responseLine = "You lose! Rock beats scissors."
+                object = 1;
             }
         } else if (computerChoice == "Paper") {
             if (humanChoice == "Scissors") {
                 humanScore++;
                 responseLine = "You Win! Scissors beats paper."
+                object = 3;
             } else if (humanChoice == "Rock") {
                 computerScore++;
                 responseLine = "You lose! Paper beats rock.";
+                object = 2;
             }
         } else if (computerChoice == "Scissors") {
             if (humanChoice == "Rock") {
                 humanScore++;
                 responseLine = "You Win! Rock beats scissors.";
+                object = 1;
             } else if (humanChoice == "Paper") {
                 computerScore++;
                 responseLine = "You lose! Scissors beats paper.";
+                object = 3;
             }
+        }
+        if (object == 1) {
+            image.src = "images/win-rock.jpg";
+            image.style = "border:5px solid black; border-radius:8px";
+        } else if (object == 2) {
+            image.src = "images/win-paper.jpg";
+            image.style = "border:5px solid black; border-radius:8px";
+        } else if (object == 3) {
+            image.src = "images/win-scissors.jpg";
+            image.style = "border:5px solid black; border-radius:8px";
         }
         response.textContent = responseLine;
         scoreboard.textContent = `Player: ${humanScore}   Computer: ${computerScore}`;
         if (humanScore == 5) {
             banner.textContent = "You won the game!";
+            image.src = "images/you-win.jpg";
+            image.style = "border:5px solid black; border-radius:8px";
         } else if (computerScore == 5) {
             banner.textContent = "You lost the game :(";
+            image.src = "images/you-lose.jpg";
+            image.style = "border:5px solid black; border-radius:8px";
         }
     }
 }
